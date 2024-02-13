6e93e854c6d8cc588b5a009b33de9c3ba068c12f
Devesh Kumar <portretrieve@yahoo.com>
2024-02-13 14:26:13 -0700

    updated bottletype argument

--- src/main/java/com/devesh/DesignPatterns/FactoryPattern/FactoryPatternNavin/BottleFactory_Navin.java
+++ src/main/java/com/devesh/DesignPatterns/FactoryPattern/FactoryPatternNavin/BottleFactory_Navin.java
@@ -6,8 +6,8 @@ import com.devesh.DesignPatterns.FactoryPattern.Bottles.GlassBottle;
 import com.devesh.DesignPatterns.FactoryPattern.Bottles.SteelBottle;
 
 public class BottleFactory_Navin {
-    public Bottle getBottle(String type){
-        switch (type){
+    public Bottle getBottle(String bottleType){
+        switch (bottleType){
             case "glass": return new GlassBottle();
             case "steel": return new SteelBottle();
             default: return new CopperBottle();
